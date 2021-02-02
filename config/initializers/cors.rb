a5498b493eba089df47b1d1d4dd8c5d5cef1224f
salmaQueen <salmawasee9@gmail.com>
2021-02-02 14:33:23 +0300

    updated

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*'
+      origins 'https://ancient-caverns-87573.herokuapp.com'
       resource '*', headers: :any, methods: [:get, :post, :patch, :put]
     end
   end
